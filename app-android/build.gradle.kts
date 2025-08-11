2fd127f0118a041d42e2d7d611ed6e1b38717159
Volodymyr Nevmerzhytskyi <azazellj@gmail.com>
2025-08-11 10:17:21 +0300

    initial release: JWTDecode

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -8,10 +8,10 @@ plugins {
 }
 
 android {
-    namespace = "dev.sdkforge.template.android"
+    namespace = "dev.sdkforge.jwt.decode.android"
     compileSdk = 36
     defaultConfig {
-        applicationId = "dev.sdkforge.template.android"
+        applicationId = "dev.sdkforge.jwt.decode.android"
         minSdk = 21
         targetSdk = 36
         versionCode = 1
---
+++ app-android/src/main/java/dev/sdkforge/jwt/decode/android/MainActivity.kt
@@ -0,0 +1,24 @@
+package dev.sdkforge.jwt.decode.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import dev.sdkforge.jwt.decode.app.App
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(
+        savedInstanceState: Bundle?,
+    ) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            App(
+                modifier = Modifier
+                    .fillMaxSize(),
+            )
+        }
+    }
+}
