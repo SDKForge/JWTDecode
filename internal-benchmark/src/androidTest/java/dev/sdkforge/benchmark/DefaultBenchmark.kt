c51c7b2c2936671949f38eafbfe876eb2184dae4
Volodymyr Nevmerzhytskyi <azazellj@gmail.com>
2025-08-28 21:44:25 +0300

    #4: Add microbenchmark for the parsing measuring

--- internal-benchmark/src/androidTest/java/dev/sdkforge/benchmark/DefaultBenchmark.kt
+++ internal-benchmark/src/androidTest/java/dev/sdkforge/benchmark/DefaultBenchmark.kt
@@ -4,6 +4,7 @@ import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import dev.sdkforge.jwt.decode.domain.JWT
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +16,17 @@ class DefaultBenchmark {
     @get:Rule
     val benchmarkRule = BenchmarkRule()
 
+    val jwt = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9" +
+        ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0" +
+        ".jYW04zLDHfR1v7xdrW3lCGZrMIsVe0vWCfVkN2DRns2c3MN-mcp_-RE6TN9umSBYoNV-mnb31wFf8iun3fB6aDS6m" +
+        "_OXAiURVEKrPFNGlR38JSHUtsFzqTOj-wFrJZN4RwvZnNGSMvK3wzzUriZqmiNLsG8lktlEn6KA4kYVaM61" +
+        "_NpmPHWAjGExWv7cjHYupcjMSmR8uMTwN5UuAwgW6FRstCJEfoxwb0WKiyoaSlDuIiHZJ0cyGhhEmmAPiCwtPAwGeaL1yZMcp0p82cpTQ5Qb" +
+        "-7CtRov3N4DcOHgWYk6LomPR5j5cCkePAz87duqyzSMpCB0mCOuE3CU2VMtGeQ"
+
     @Test
-    fun measure() {
+    fun measureRS512JWTParsing() {
         benchmarkRule.measureRepeated {
-            // TODO: add measurables here
+            JWT(jwt)
         }
     }
 }
